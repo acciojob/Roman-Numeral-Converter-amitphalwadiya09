d759190fbc5e145de8dfeaf559c3799371317ce2
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-24 11:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
     };
 
-  //your code here
+    let result = '';
 
+    for (let key in obj) {
+        const symbol = obj[key][0];
+        const value = obj[key][1];
+
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+
+        // Check for subtractive notation
+        const nextKey = (key - 2) >= 0 ? key - 2 : key - 1;
+        const nextSymbol = obj[nextKey][0];
+        const nextValue = obj[nextKey][1];
+
+        if (nextKey >= 0 && num >= (value - nextValue)) {
+            result += nextSymbol + symbol;
+            num -= (value - nextValue);
+        }
+    }
+
+    return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
+// You can test your code by running the above function and printing it to the console.
+// To run it with input 36, uncomment the following line
 // console.log(convertToRoman(36));
 
+// Do not edit below this line
+module.exports = convertToRoman;
 
-
-
-// do not edit below this line
-module.exports = convertToRoman
